0fafec88336964e74382ea6ab2f6af8e141d5339
Linker4 <61056244+domster704@users.noreply.github.com>
2023-02-17 11:59:45 +0300

    Update rational.hpp

--- isupov_g_s/prj.lab/rational/include/rational/rational.hpp
+++ isupov_g_s/prj.lab/rational/include/rational/rational.hpp
@@ -1,1 +1,72 @@
+#include <iosfwd>
+#include "stdexcept"
+#include "numeric"
 
+class Rational {
+public:
+    Rational() = default;
+
+    Rational(const Rational& r) = default;
+
+    Rational(int64_t num, int64_t denom);
+
+    ~Rational() = default;
+
+    Rational& operator=(const Rational& r) = default;
+
+    Rational& operator+=(const Rational& r);
+
+    Rational& operator-=(const Rational& r);
+
+    Rational& operator*=(const Rational& r);
+
+    Rational& operator/=(const Rational& r);
+
+    Rational& operator++();
+
+    Rational operator++(int);
+
+    Rational& operator--();
+
+    Rational operator--(int);
+
+    bool operator==(const Rational& r);
+
+    bool operator!=(const Rational& r);
+
+    bool operator>(const Rational& r);
+
+    bool operator<(const Rational& r);
+
+    bool operator>=(const Rational& r);
+
+    bool operator<=(const Rational& r);
+
+    std::istream& ReadFrom(std::istream& istream);
+
+    inline std::ostream& WriteTo(std::ostream& ostream) const;
+
+    int64_t GetNum() const;
+
+    int64_t GetDenum() const;
+
+private:
+    int64_t num = 1;
+    int64_t denom = 1;
+
+    void simplify();
+};
+
+Rational operator-(const Rational& r);
+
+Rational operator+(const Rational& l, const Rational& r);
+
+Rational operator-(const Rational& l, const Rational& r);
+
+Rational operator*(const Rational& l, const Rational& r);
+
+Rational operator/(const Rational& l, const Rational& r);
+
+std::ostream& operator<<(std::ostream& ostream, const Rational& r);
+
+std::istream& operator>>(std::istream& istream, Rational& r);
